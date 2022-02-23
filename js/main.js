cdab8286b8462d7525874b3af85c14356c67c686
Siwei Liu <liusiwei.yjlo@gmail.com>
2022-02-22 20:18:37 -0500

    Initial push

---
+++ js/main.js
@@ -0,0 +1,100 @@
+let runtime = runtimeExecuter();
+let evaluator = runtimeEvaluator();
+let parser = runtimeParser();
+
+let enterCallback = null;
+let cursor = 0;
+let row = 0;
+let word = "";
+let gameover = false;
+
+let io = {
+	Write: (text, style) => {
+        if (text.trim() === "win") {
+            gameover = true;
+            return;
+        }
+        try {
+            let data = JSON.parse(text);
+            for (let i = 0; i < 5; i++) {
+                let classColor = "gray";
+                if (data[i] === 1) {
+                    classColor = "green";
+                } else if (data[i] === 2) {
+                    classColor = "yellow";
+                }
+                $('.r:nth-child(' + (row+1) + ') .c:nth-child('+(i+1)+')').addClass(classColor);
+                $('.key-'+word[i]).addClass(classColor);
+            }
+            if (row < 5) {
+                // reset
+                row += 1;
+                word = "";
+                cursor = 0;
+            }
+           
+        } catch (ex) {
+            console.log(text);
+        }
+    },
+	Input: (callback)=>{
+        enterCallback = callback;
+    },
+    AbortInput: ()=>{}
+}
+
+window.addEventListener("keydown", function(e) {
+    if (gameover) {
+        return;
+    }
+    if(e.code === "Enter") {
+        if (word.length === 5) {
+            if (words.indexOf(word) === -1) {
+                $( ".alert" ).show();
+                $( ".alert" ).delay(1000).fadeOut( "slow" );
+            } else {
+                enterCallback(word);
+            }
+        }
+    	// prevent scroll
+        e.preventDefault();
+    } else if ('abcdefghijklmnopqrstuvwxyz'.indexOf(e.key) > -1) {
+        if (cursor < 5) {
+            word += e.key;
+            $('.r:nth-child(' + (row+1) + ') .c:nth-child('+(cursor+1)+')').text(e.key.toUpperCase());
+            cursor += 1;
+        }
+    } else if (e.key === "Backspace") {
+        if (cursor > 0) {
+            cursor -= 1;
+            $('.r:nth-child(' + (row+1) + ') .c:nth-child('+(cursor+1)+')').text("");
+            word = word.slice(0, word.length-1)
+        }
+    }
+    
+}, false);
+
+// parser, evaluater, editor, consl, canvas, controls, options
+runtime.config(parser, evaluator, null, io, null, {}, {});
+
+const randomWord = words[Math.floor(Math.random() * words.length)];
+
+runtime.restart();
+runtime.executeAll({in: words}, rundisSrc);
+
+let keyboard = $(".keyboard");
+let keys = [
+    "QWERTYUIOP",
+    "ASDFGHJKL",
+    "ZXCVBNM"
+];
+
+for (let r = 0; r < keys.length; r++) {
+    let row = keys[r];
+    let keyRow = $('<div class="key-row"></div>');
+    for (let i = 0; i < row.length; i++) {
+        let letter = row[i];
+        keyRow.append($('<div class="key key-'+letter.toLowerCase()+'">'+letter+'</div>'));
+    }
+    keyboard.append(keyRow);
+}
---
+++ js/source.js
@@ -0,0 +1,43 @@
+let rundisSrc = `
+let list $in
+len $list count
+rnd idx 0 $count
+
+get $list $idx w
+
+let map {}
+for c $w
+ put $map $c 1
+nxt
+
+#loop
+inp in
+
+let res []
+let win 1
+for i 5
+ get $in $i input_c
+ get $w $i true_c
+ ife $input_c $true_c
+  psh $res 1
+ els
+  get $map $input_c contains
+  ife $contains $nil
+   psh $res 0
+   let win 0
+  els
+   psh $res 2
+   let win 0
+  fin
+ fin
+nxt
+
+/ prt $w
+prt $res
+
+ife $win 1
+ prt 'win'
+els
+ jmp loop
+fin
+`
